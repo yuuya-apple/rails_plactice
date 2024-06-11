b72bb7f677bf13b6478921463ff9cc84a4d76d12
kawa <sample@exe.com>
2024-06-12 02:33:35 +0900

    セッションのコミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@
 
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_required
 
   private
 
   def current_user
-    @currnt_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to(login_url) unless current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'session#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   namespace :admin do
     resources :users
